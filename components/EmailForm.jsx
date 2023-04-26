3a7f04200d08d9861807321174beab1ae1a116f1
fsanchez <felipesanchez10@protonmail.com>
2023-04-25 21:33:00 -0400

    formCSS

--- components/EmailForm.jsx
+++ components/EmailForm.jsx
@@ -25,44 +25,48 @@ function EmailForm() {
     e.target.reset();
   };
   return (
-    <div className="rounded-md border-2 border-red-200 p-2 flex justify-center z-50">
+    <div className=" absolute right-0 left-0 m-auto w-[380px]  p-2 flex justify-center z-50">
       <form
         ref={form}
         onSubmit={sendEmail}
-        className="p-4 w-[380px] border-2 border-red-400 "
+        className="relative bg-[#3a434c] p-4 sm:w-[380px]  border-2 border-[#af5c5c] rounded-sm h-[500px]"
       >
-        <div className="m-1 flex justify-between">
-          <span>Your Email: </span>
+        <div className="m-1 flex justify-between ">
+          <span className="md: mr-2">Your Email: </span>
           <input
-            className=" px-2 border-2 border-red-600 text-black"
+            className=" px-2 rounded-sm border-gray-600 border-[1px] transition ease-out focus:outline-none focus:border-gray-800 text-black"
             name="user_email"
             type="text"
             placeholder="ex@gmail.com"
             required
           />
         </div>
-        <div className="m-1 flex justify-between">
-          <span>Subject: </span>
+        {/* <div className="m-1 flex justify-between md:justify-start">
+          <span className="md: mr-2">Subject: </span>
           <input
             name="subject"
             type="text"
             placeholder="Company Name"
-            className="px-2 border-2 border-red-600 text-black"
+            className="px-2 border-gray-600 border-[1px] transition ease-out focus:outline-none focus:border-gray-800 rounded-sm text-black"
           />
-        </div>
-        <div className="m-1 flex justify-between">
-          <span className="block">Message: </span>
+        </div> */}
+        <div className="m-1 flex justify-between ">
+          <span className="block md: mr-2">Message: </span>
           <textarea
             name="message"
             cols="24"
             rows="5"
             placeholder="What do you need help with?"
-            className=" px-2 border-2 border-red-600 text-black"
+            className=" px-2 border-gray-600 border-[1px] transition ease-out focus:outline-none focus:border-gray-800 rounded-sm text-black"
             required
           ></textarea>
         </div>
-        <div className="flex justify-center ">
-          <button className="border-2 border-red-800 w-full" type="submit">
+
+        <div className="absolute bottom-6 left-0 right-0 m-auto flex justify-center">
+          <button
+            className="bg-[#af5c5c] border-2 border-[#af5c5c] md:w-[150px] w-[120px] rounded-sm "
+            type="submit"
+          >
             Send
           </button>
         </div>
